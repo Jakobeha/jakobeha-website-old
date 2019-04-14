070b83d7e8c185148528812cfd76b6741e63e33d
jakob <jakobeha@gmail.com>
2019-04-14 17:46:49 -0400

    ready for github (?)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,16 +2,12 @@ import Bio from '!raw-loader!src/content/bio.md';
 import Contact from "!raw-loader!src/content/contact.md";
 import Experience from "!raw-loader!src/content/experience.md";
 import RIR from '!raw-loader!src/content/rir.md';
-import Unit1 from "!raw-loader!src/content/unit1.md";
-import Unit3 from '!raw-loader!src/content/unit3.md';
-import Unit4 from '!raw-loader!src/content/unit4.md';
+import TreeScript from '!raw-loader!src/content/treeScript.md';
 import * as React from 'react';
 import Entry from 'src/components/Entry';
 import Section, { SectionStyle } from 'src/components/Section';
 import Background from "src/resources/background.png";
 import ResumeLink from 'src/resources/resume.pdf';
-import Unit1Link from "src/resources/unit1.pdf";
-import Unit3Link from 'src/resources/unit3.pdf';
 import 'src/styles/App.css';
 import FancyText from 'src/util/FancyText';
 
@@ -25,30 +21,16 @@ export default function App({}: {}): JSX.Element {
           <Entry
             title="RIR"
             link="https://github.com/reactorlabs/rir"
-            date="January - Now"
+            date="January 2019 - Now"
           >
             <FancyText content={RIR} inline={false} />
           </Entry>
           <Entry
-            title="Programming Language Comparison"
-            link="https://feature-compare-qe06piape.now.sh/"
-            date="March - April"
+            title="TreeScript"
+            link="https://github.com/Jakobeha/treescript"
+            date="December 2018 - Now"
           >
-            <FancyText content={Unit4} inline={false} />
-          </Entry>
-          <Entry
-            title="Unit 3 Proposal"
-            link={Unit3Link}
-            date="March"
-          >
-            <FancyText content={Unit3} inline={false} />
-          </Entry>
-          <Entry
-            title='"Clean Code" - How to Write it?'
-            link={Unit1Link}
-            date="January"
-          >
-            <FancyText content={Unit1} inline={false} />
+            <FancyText content={TreeScript} inline={false} />
           </Entry>
         </Section>
         <Section title="About" style={SectionStyle.Row}>
---
+++ src/content/treeScript.md
@@ -0,0 +1,1 @@
+TODO Summary, currently see the [README](https://github.com/Jakobeha/treescript/blob/master/README.md).
